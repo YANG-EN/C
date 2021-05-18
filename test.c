88fee23a6ee6dcf75e8e8b9732c52067cb593bbf
YANG-EN <84328216+YANG-EN@users.noreply.github.com>
2021-05-18 10:47:27 +0800

    Create test.c

---
+++ test.c
@@ -0,0 +1,13 @@
+public class test1 {
+
+    public static void main(String[] args) {
+       int i=1,sum=0;
+       
+      while(i<=100){
+           sum=sum+i;        
+           i++;
+        }
+       System.out.println("100以内的和："+sum);
+     }
+
+}
