da19e092577e80f16c59fa139bcde1b42da36c3e
YANG-EN <84328216+YANG-EN@users.noreply.github.com>
2021-05-18 10:55:28 +0800

    Create test1.java

---
+++ test1.java
@@ -0,0 +1,13 @@
+public class Test1 {
+ 
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        for(int i=1;i<10;i++){
+            if(i==5){
+              System.out.println("循环提前结束");
+               break;                
+             }
+          System.out.println("   "+i);
+        }
+     }
+ }
